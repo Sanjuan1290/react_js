759cd01b5c47daaaa0ae187bc441a71438cae932
robert <robertrenbysanjuan@gmail.com>
2025-04-27 08:38:28 +0800

    Start learning ReactJS

---
+++ react-first-project/src/index.jsx
@@ -0,0 +1,10 @@
+
+import { createRoot } from 'react-dom/client'
+
+const root = createRoot(document.querySelector('#root'))
+root.render( 
+    <ul>
+        <li>For Start up!</li>
+        <li>For Futture  </li>
+        <li>FOr OJT      </li>
+    </ul>)
