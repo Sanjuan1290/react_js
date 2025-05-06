24444253c51e9acf506ab5630c9e781bfa36eb5b
robert <robertrenbysanjuan@gmail.com>
2025-05-06 08:55:53 +0800

    lesson 13 section 2

--- section_project_part1/src/App.jsx
+++ section_project_part1/src/App.jsx
@@ -1,13 +1,12 @@
-
 import { createRoot } from 'react-dom/client'
-import Header from './components/Header.jsx'
-import Main from './components/Main.jsx'
-
-
+import Joke from './components/joke.jsx'
 
 createRoot(document.querySelector('#root')).render(
-    <div className="container">
-        <Header />
-        <Main />
-    </div>
+    <>
+        <Joke setup="WHY DID THE DEVELOPER GO BROKE?" punchline="BECAUSE HE USED UP ALL HIS CACHE."/>
+        <Joke setup="WHAT DID ONE HTML TAG SAY TO THE OTHER?" punchline="I REALLY FEEL LIKE YOU’RE OPENING UP TO ME."/>
+        <Joke setup="WHY DON’T SKELETONS FIGHT EACH OTHER?" punchline="THEY DON’T HAVE THE GUTS."/>
+        <Joke setup="BECAUSE THE WIFI SIGNAL WAS STRONGER IN THE FRIDGE." />
+        <Joke setup="I TOLD MY COMPUTWER I NEEDED A BREAK — IT CRASHED." />
+    </>
 )
---
+++ section_project_part1/src/components/joke.jsx
@@ -0,0 +1,6 @@
+
+export default function(props){
+    return(
+        <p>"{props.setup} {props.punchline}"</p>
+    )
+}
