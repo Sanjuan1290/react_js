237ceb05cbe2412443039d0b6239946a47428f6f
robert <robertrenbysanjuan@gmail.com>
2025-05-31 09:41:34 +0800

    add: about page and styled it

--- React_Router/src/components/About.jsx
+++ React_Router/src/components/About.jsx
@@ -2,6 +2,29 @@
 export default function About(){
 
     return(
-        <p>About</p>
+        <section className="aboutPage">
+
+            <img src="/aboutImage.png" alt="about image" />
+
+            <section className="content-container">
+                <h1>Don't squeeze in a sedan when you could relax in a van.</h1>
+
+                <small>Our mission is to enliven your road trip with the perfect travel van rental.
+                    Our vans are recertified before each trip to ensure your travel plans can go off without a hitch.
+                    (Hitch costs extra 😫)
+                </small>
+
+                <small>Our team is full of vanlife enthusiasts who know firsthand the magic of touring the world on 4 wheels.</small>
+
+                <div>
+                    <h2>
+                        Your destination is waiting.
+                        Your van is ready.
+                    </h2>
+
+                    <button>Explore our Vans</button>
+                </div>
+            </section>
+        </section>
     )
 }
